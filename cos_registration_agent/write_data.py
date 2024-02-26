954d93b23c117394ef66851ccc7cc50b3bda67d6
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-02-26 14:41:27 +0100

    add docstring

--- cos_registration_agent/write_data.py
+++ cos_registration_agent/write_data.py
@@ -11,17 +11,18 @@ def write_data(data, filename, folder='SNAP_COMMON'):
     - data (str): The data to be written to the file.
     - filename (str): The name of the file to which the data will be written.
     - folder (str, optional): The folder in which the file will be saved.
+    Returns:
+    - None: if writing successfull.
     """
     try:
-        snap_common_dir = os.environ.get(folder)
+        dir = os.environ.get(folder)
     except KeyError as e:
         raise e
 
-    file_path = os.path.join(snap_common_dir, f'{filename}')
+    file_path = os.path.join(dir, f'{filename}')
     try:
         with open(file_path, 'w') as file:
             file.write(data)
-            return True
     except OSError as e:
         logger.error(f"Failed to open {file_path}.")
         raise e

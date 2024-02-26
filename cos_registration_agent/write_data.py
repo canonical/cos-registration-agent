dc2ae9b6f0228b8a884cb20a049bd689721c2f45
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-02-26 14:30:57 +0100

    add docstring

--- cos_registration_agent/write_data.py
+++ cos_registration_agent/write_data.py
@@ -5,6 +5,13 @@ logger = logging.getLogger(__name__)
 
 
 def write_data(data, filename, folder='SNAP_COMMON'):
+    """
+    Write data to a file.
+    Args:
+    - data (str): The data to be written to the file.
+    - filename (str): The name of the file to which the data will be written.
+    - folder (str, optional): The folder in which the file will be saved.
+    """
     try:
         snap_common_dir = os.environ.get(folder)
     except KeyError as e:

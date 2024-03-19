4f7238f21a3275fd00475d34cb03f52612a00622
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-03-19 09:14:14 +0100

    fix linting

--- cos_registration_agent/write_data.py
+++ cos_registration_agent/write_data.py
@@ -10,20 +10,16 @@ def write_data(data, filename, folder):
     Args:
     - data (str): The data to be written to the file.
     - filename (str): The name of the file to which the data will be written.
-    - folder (str, optional): The folder in which the file will be saved. If the 
-      folder is a SNAP env path such as $SNAP_COMMON, it will be detected and
-      the path built accordingly
+    - folder (str, optional): The folder in which the file will be saved.
+      If the folder is a SNAP env path such as $SNAP_COMMON,
+      it will be detected and the path built accordingly
     Returns:
     - None: if writing successfull.
     """
-    try:
-      file_path = os.path.join(folder, filename)
-    except Exception as e:
-      logging.error(f"Failed to join folder and filename: {folder}, {filename}.")
-      raise e
+    file_path = os.path.join(folder, filename)
 
     try:
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             file.write(data)
     except OSError as e:
         logger.error(f"Failed to open {file_path}.")

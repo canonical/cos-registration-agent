e3956dbaf6cba9d85d698a6a89ffa5458645607a
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2025-01-23 18:44:03 +0100

    feat(snap): add rule files from loki and prometheus to register and update

--- snap/local/register-device.sh
+++ snap/local/register-device.sh
@@ -7,6 +7,12 @@ if [ ! -f "$CONFIGURATION_FILE_PATH" ]; then
     exit 1
 fi
 
-$SNAP/bin/cos-registration-agent --shared-data-path $SNAP_COMMON/rob-cos-shared-data --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts setup -c $CONFIGURATION_FILE_PATH
+$SNAP/bin/cos-registration-agent \
+  --shared-data-path $SNAP_COMMON/rob-cos-shared-data \
+  --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards \
+  --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts \
+  --loki-rule-files $SNAP_COMMON/configuration/loki_alert_rules \
+  --prometheus-rule-files $SNAP_COMMON/configuration/prometheus_alert_rules \
+  setup -c $CONFIGURATION_FILE_PATH
 
 snapctl start --enable ${SNAP_NAME}.update-device-configuration 2>&1
--- snap/local/update-device-configuration.sh
+++ snap/local/update-device-configuration.sh
@@ -2,4 +2,10 @@
 
 CONFIGURATION_FILE_PATH="$SNAP_COMMON/configuration/device.yaml"
 
-$SNAP/bin/cos-registration-agent --shared-data-path $SNAP_COMMON/rob-cos-shared-data --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts update -c $CONFIGURATION_FILE_PATH
+$SNAP/bin/cos-registration-agent \
+  --shared-data-path $SNAP_COMMON/rob-cos-shared-data \
+  --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards \
+  --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts \
+  --loki-rule-files $SNAP_COMMON/configuration/loki_alert_rules \
+  --prometheus-rule-files $SNAP_COMMON/configuration/prometheus_alert_rules \
+  update -c $CONFIGURATION_FILE_PATH

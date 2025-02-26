13e4d16e1a5473ddc77e7e3ab26e5907bdd3f066
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2025-02-26 17:56:48 +0100

    allow registering device even if grafana_dahsboard and foxglove layouts are not present in the configuration

--- snap/local/register-device.sh
+++ snap/local/register-device.sh
@@ -7,6 +7,19 @@ if [ ! -f "$CONFIGURATION_FILE_PATH" ]; then
     exit 1
 fi
 
-$SNAP/bin/cos-registration-agent --shared-data-path $SNAP_COMMON/rob-cos-shared-data --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts setup -c $CONFIGURATION_FILE_PATH
+REGISTRATION_CMD="$SNAP/bin/cos-registration-agent --shared-data-path $SNAP_COMMON/rob-cos-shared-data"
+
+# Check if grafan_dashboards directory exists in configuration
+if [ -d "$SNAP_COMMON/configuration/grafana_dashboards" ]; then
+    REGISTRATION_CMD="$REGISTRATION_CMD --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards"
+fi
+
+# Check if foxglove_layouts directory exists in configuration
+if [ -d "$SNAP_COMMON/configuration/foxglove_layouts" ]; then
+    REGISTRATION_CMD="$REGISTRATION_CMD --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts"
+fi
+
+REGISTRATION_CMD="$REGISTRATION_CMD setup -c $CONFIGURATION_FILE_PATH"
+eval "$REGISTRATION_CMD"
 
 snapctl start --enable ${SNAP_NAME}.update-device-configuration 2>&1

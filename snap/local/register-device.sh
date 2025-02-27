ca5b068c61962194020a8e925d01b859e6e2ef40
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2025-02-27 11:01:42 +0100

    clean up

--- snap/local/register-device.sh
+++ snap/local/register-device.sh
@@ -1,25 +1,29 @@
 #!/bin/sh -e
 
-CONFIGURATION_FILE_PATH=$SNAP_COMMON/configuration/device.yaml
+CONFIGURATION_FILE_PATH=${SNAP_COMMON}/configuration/device.yaml
 
-if [ ! -f "$CONFIGURATION_FILE_PATH" ]; then
-    echo "Configuration file '$CONFIGURATION_FILE_PATH' does not exist."
+if [ ! -f "${CONFIGURATION_FILE_PATH}" ]; then
+    echo "Configuration file '${CONFIGURATION_FILE_PATH}' does not exist."
     exit 1
 fi
 
-REGISTRATION_CMD="$SNAP/bin/cos-registration-agent --shared-data-path $SNAP_COMMON/rob-cos-shared-data"
+# Set the registration command args based on configuration
+REGISTRATION_CMD_ARGS=""
 
 # Check if grafan_dashboards directory exists in configuration
-if [ -d "$SNAP_COMMON/configuration/grafana_dashboards" ]; then
-    REGISTRATION_CMD="$REGISTRATION_CMD --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards"
+if [ -d "${SNAP_COMMON}/configuration/grafana_dashboards" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --grafana-dashboards ${SNAP_COMMON}/configuration/grafana_dashboards"
 fi
 
 # Check if foxglove_layouts directory exists in configuration
-if [ -d "$SNAP_COMMON/configuration/foxglove_layouts" ]; then
-    REGISTRATION_CMD="$REGISTRATION_CMD --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts"
+if [ -d "${SNAP_COMMON}/configuration/foxglove_layouts" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --foxglove-studio-dashboards ${SNAP_COMMON}/configuration/foxglove_layouts"
 fi
 
-REGISTRATION_CMD="$REGISTRATION_CMD setup -c $CONFIGURATION_FILE_PATH"
-eval "$REGISTRATION_CMD"
+# Build the registration command with the args
+REGISTRATION_CMD="${SNAP}/bin/cos-registration-agent --shared-data-path ${SNAP_COMMON}/rob-cos-shared-data"
+REGISTRATION_CMD="${REGISTRATION_CMD} ${REGISTRATION_CMD_ARGS} setup -c ${CONFIGURATION_FILE_PATH}"
+
+eval "${REGISTRATION_CMD}"
 
 snapctl start --enable ${SNAP_NAME}.update-device-configuration 2>&1

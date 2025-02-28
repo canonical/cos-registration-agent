27777dbbe131ef44d3a3dc55d8eb034d7bfe5142
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2025-02-28 10:31:02 +0100

    make update device configuration more flexible with args

--- snap/local/update-device-configuration.sh
+++ snap/local/update-device-configuration.sh
@@ -2,4 +2,15 @@
 
 CONFIGURATION_FILE_PATH="$SNAP_COMMON/configuration/device.yaml"
 
-$SNAP/bin/cos-registration-agent --shared-data-path $SNAP_COMMON/rob-cos-shared-data --grafana-dashboards $SNAP_COMMON/configuration/grafana_dashboards --foxglove-studio-dashboards $SNAP_COMMON/configuration/foxglove_layouts update -c $CONFIGURATION_FILE_PATH
+# Check if grafan_dashboards directory exists in configuration
+if [ -d "${SNAP_COMMON}/configuration/grafana_dashboards" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --grafana-dashboards ${SNAP_COMMON}/configuration/grafana_dashboards"
+fi
+
+# Check if foxglove_layouts directory exists in configuration
+if [ -d "${SNAP_COMMON}/configuration/foxglove_layouts" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --foxglove-studio-dashboards ${SNAP_COMMON}/configuration/foxglove_layouts"
+fi
+
+# Call the update command with the args
+${SNAP}/bin/cos-registration-agent --shared-data-path ${SNAP_COMMON}/rob-cos-shared-data ${REGISTRATION_CMD_ARGS} update -c ${CONFIGURATION_FILE_PATH}

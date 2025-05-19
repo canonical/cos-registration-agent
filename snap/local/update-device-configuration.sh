1d03f5d87849185376486d952b26f14f2a81e7c5
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2025-05-19 19:14:08 +0200

    fix path set to setup dashboards

--- snap/local/update-device-configuration.sh
+++ snap/local/update-device-configuration.sh
@@ -18,28 +18,31 @@ fi
 echo "Using configuration file: ${CONFIGURATION_FILE_PATH}."
 logger -t ${SNAP_NAME} "Using configuration file: ${CONFIGURATION_FILE_PATH}."
 
+# Retrieve the directory at which the configuration is stored
+CONFIGURATION_DIR_PATH="$(dirname "${CONFIGURATION_FILE_PATH}")"
+
 # Set the registration command args based on configuration
 REGISTRATION_CMD_ARGS=""
 
 # Check if grafan_dashboards directory exists in configuration
-if [ -d "${CONFIGURATION_FILE_PATH}/grafana_dashboards" ]; then
-    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --grafana-dashboards ${CONFIGURATION_FILE_PATH}/grafana_dashboards"
+if [ -d "${CONFIGURATION_DIR_PATH}/grafana_dashboards" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --grafana-dashboards ${CONFIGURATION_DIR_PATH}/grafana_dashboards"
 fi
 
 # Check if foxglove_layouts directory exists in configuration
-if [ -d "${CONFIGURATION_FILE_PATH}/foxglove_layouts" ]; then
-    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --foxglove-studio-dashboards ${CONFIGURATION_FILE_PATH}/foxglove_layouts"
+if [ -d "${CONFIGURATION_DIR_PATH}/foxglove_layouts" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --foxglove-studio-dashboards ${CONFIGURATION_DIR_PATH}/foxglove_layouts"
 fi
 
 # Check if loki_alert_rules directory exists in configuration
-if [ -d "${CONFIGURATION_FILE_PATH}/loki_alert_rules" ]; then
-    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --loki-alert-rule-files ${CONFIGURATION_FILE_PATH}/loki_alert_rules"
+if [ -d "${CONFIGURATION_DIR_PATH}/loki_alert_rules" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --loki-alert-rule-files ${CONFIGURATION_DIR_PATH}/loki_alert_rules"
 fi
 
 # Check if prometheus_alert_rules directory exists in configuration
-if [ -d "${CONFIGURATION_FILE_PATH}/prometheus_alert_rules" ]; then
-    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --prometheus-alert-rule-files ${CONFIGURATION_FILE_PATH}/prometheus_alert_rules"
+if [ -d "${CONFIGURATION_DIR_PATH}/prometheus_alert_rules" ]; then
+    REGISTRATION_CMD_ARGS="${REGISTRATION_CMD_ARGS} --prometheus-alert-rule-files ${CONFIGURATION_DIR_PATH}/prometheus_alert_rules"
 fi
 
 # Call the update command with the args
-${SNAP}/bin/cos-registration-agent --shared-data-path ${SNAP_COMMON}/rob-cos-shared-data ${REGISTRATION_CMD_ARGS} update -c ${CONFIGURATION_FILE_PATH}
+${SNAP}/bin/cos-registration-agent --shared-data-path ${SNAP_COMMON}/rob-cos-shared-data ${REGISTRATION_CMD_ARGS} update -c ${CONFIGURATION_DIR_PATH}
